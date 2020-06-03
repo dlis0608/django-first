7a99cd7b0c0adba43b69e860eee2e58362f2b6d3
Лариса <dlis0608@gmail.com>
2020-06-03 23:36:22 +0300

    Создан шаблон для вывода категорий

--- firstsite/news/views.py
+++ firstsite/news/views.py
@@ -7,3 +7,10 @@ def index(request):
     news = News.objects.all()
     categories = Category.objects.all()
     return render(request, 'news/index.html', {'news': news, 'categories': categories, 'title': 'Список новостей'})
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category': category})
