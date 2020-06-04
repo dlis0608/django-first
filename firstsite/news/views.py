c004de62d004a736bb5639bf2bf8d2339457b797
Лариса <dlis0608@gmail.com>
2020-06-04 15:17:20 +0300

    Добавлен пользовательский тег для вывода сптиска категорий

--- firstsite/news/views.py
+++ firstsite/news/views.py
@@ -5,12 +5,10 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
-    return render(request, 'news/index.html', {'news': news, 'categories': categories, 'title': 'Список новостей'})
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category': category})
+    return render(request, 'news/category.html', {'news': news, 'category': category})
