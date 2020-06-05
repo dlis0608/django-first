2c5a20925439334a25d9e7ab7e0517d7a781808d
Лариса <dlis0608@gmail.com>
2020-06-05 12:52:34 +0300

    Добавлена форма добавлкения записей, не связанная с моделью

--- firstsite/firstsite/settings.py
+++ firstsite/firstsite/settings.py
@@ -113,6 +113,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'firstsite/static'),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
