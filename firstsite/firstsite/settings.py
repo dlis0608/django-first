fa14c8cf8eaef35b87fb1fc6be38ee50a1e449c1
Лариса <dlis0608@gmail.com>
2020-06-12 01:21:35 +0300

    Настроена отправка писем на email - уточнение

--- firstsite/firstsite/settings.py
+++ firstsite/firstsite/settings.py
@@ -133,6 +133,6 @@ INTERNAL_IPS = ['127.0.0.1']
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = '465'
 EMAIL_HOST_USER = 'webkoshechka@yandex.ru'
-EMAIL_HOST_PASSWORD = 'vfndttxrfrjntyjxtr0809'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
