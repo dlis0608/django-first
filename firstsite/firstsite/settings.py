3abffd0cdb64915354cb64d07e1509a8fa7acdd9
Лариса <dlis0608@gmail.com>
2020-06-12 03:11:28 +0300

    Работа с кешем, финал проекта

--- firstsite/firstsite/settings.py
+++ firstsite/firstsite/settings.py
@@ -206,3 +206,8 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+CACHES = {
+    'default': dict(BACKEND='django.core.cache.backends.filebased.FileBasedCache',
+                    LOCATION=os.path.join(BASE_DIR, 'cache'), TIMEOUT=60)
+}
